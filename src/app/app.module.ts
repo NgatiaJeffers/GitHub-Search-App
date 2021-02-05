39436de5aa2def8b633dc2260ef85278c5c98ede
Jefferson Gakuya <jeffersongakuya@gmail.com>
2021-02-06 01:06:26 +0300

    Added component for user data and repos plus class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDataComponent } from './user-data/user-data.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { UserRepoComponent } from './user-repo/user-repo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDataComponent,
+    SearchBarComponent,
+    NotFoundComponent,
+    UserRepoComponent
   ],
   imports: [
     BrowserModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  myKey: '8b9e17143d094a525a75e0d0637d4f7691770cb4'
 };
 
 /*
