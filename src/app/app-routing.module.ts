08ae4f2faa0a7cc951c0b70899043b649d879d1e
Jefferson Gakuya <jeffersongakuya@gmail.com>
2021-02-06 02:32:04 +0300

    Added navbar and search bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { UserRepoComponent } from './user-repo/user-repo.component';
+import { UserDataComponent } from './user-data/user-data.component';
+import { NotFoundComponent } from './not-found/not-found.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'user-data', component: UserDataComponent },
+  { path: 'user-repo', component: UserRepoComponent },
+  { path: '**', component: NotFoundComponent },
+
+  // Empty Paths
+  { path: '', redirectTo: "/user-data", pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
